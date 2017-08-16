02cf3c29caad928aec53bfd24e53c83b6dac8ead
AndrewM <amourcos@gmail.com>
2017-08-16 16:03:26 -0400

    Changed operations from permutations to cartesian product

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
-from itertools import permutations
+from itertools import permutations, product
 
+file = open("output.txt", "w")
 
 def reverse_polish(exp):
     stack = []
@@ -25,21 +26,47 @@ def reverse_polish(exp):
 
     return stack.pop()
 
-nums = []
+nums4 = []
+nums3 = []
+nums2 = []
+nums1 = []
 expression = []
 
-perm1 = permutations(['1', '4', '8', '9'], 4)
+perm4 = permutations(['1', '4', '8', '9'], 4)
+for p in perm4:
+    nums4.append(p)
+perm3 = permutations(['1', '4', '8', '9'], 3)
+for p in perm3:
+    nums3.append(p)
+perm2 = permutations(['1', '4', '8', '9'], 2)
+for p in perm2:
+    nums2.append(p)
+perm1 = permutations(['1', '4', '8', '9'], 1)
 for p in perm1:
-    nums.append(p)
+    nums1.append(p)
 
-for item in nums:
-    perm2 = permutations(['+','-','/','*'], 3)
-    for p in perm2:
+for item in nums4:
+    OpsPerm4 = product(['+','-','/','*'], repeat=3)
+    for p in OpsPerm4:
+        expression.append(item+p)
+for item in nums3:
+    OpsPerm3 = product(['+','-','/','*'], repeat=2)
+    for p in OpsPerm3:
+        expression.append(item+p)
+for item in nums2:
+    OpsPerm2 = product(['+','-','/','*'], repeat=1)
+    for p in OpsPerm2:
+        expression.append(item+p)
+for item in nums1:
+    OpsPerm1 = product(['+','-','/','*'], repeat=0)
+    for p in OpsPerm1:
         expression.append(item+p)
 
 for i in expression:
     exp = " ".join(i)
     polish_result = reverse_polish(exp)
-    if (0 <= polish_result <= 100) and (polish_result.is_integer()):
+    if (1 <= polish_result <= 100) and (polish_result.is_integer()):
+        print(i, '=', polish_result, file=file)
 
-        print(i,'=', polish_result)
+file.close()
+
